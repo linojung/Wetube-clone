d3352f1544a48ad3108d0b94f3d522202265cdf7
linojung <46038961+linojung@users.noreply.github.com>
2023-09-30 23:26:35 +0900

    created PORT

--- src/server.js
+++ src/server.js
@@ -1,1 +1,9 @@
 import express from "express";
+
+const PORT = 4000;
+const app = express();
+
+const handleListening = () =>
+  console.log(`Server listening on port http://localhost:${PORT}🦄`);
+
+app.listen(PORT, handleListening);
