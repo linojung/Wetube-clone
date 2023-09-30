842e4bc16fb2972ba389c8830434271ba8f3ff99
linojung <46038961+linojung@users.noreply.github.com>
2023-10-01 05:42:12 +0900

    respond
    res.end()
    res.send()

--- src/server.js
+++ src/server.js
@@ -3,6 +3,14 @@ import express from "express";
 const PORT = 4000;
 const app = express();
 
+//app 설정
+
+const handleHome = (req, res) => {
+  return res.send("I love you ");
+};
+
+app.get("/", handleHome);
+
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}🦄`);
 
